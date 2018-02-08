bdf5213b7cf96deeb44c545cad4a62acf39ab44e
Rey Almaraz <ChrisRey@wlan-t-195-63-dhcp.CSUChico.EDU>
2018-02-08 10:53:57 -0800

    Change characters with ASCII

--- main.cpp
+++ main.cpp
@@ -5,20 +5,30 @@ using namespace std;
 
 int main()
 {
-
+   int ASCII;
+   char characterz;
+   
   cout<<"What character do you want to know about?\n";
-
-  //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
+ 
+  cin>> characterz;
+   ASCII=characterz;
+   if ((ASCII >= 97) ||(ASCII <=122)) {
 
   //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
+  //
+    cout<<" is an lower case letter!\n";
+   }  else if ((ASCII >=65) || (ASCII <=90)) {
+
+  //when user's entry is between A-Z...
+    cout<<" is a upper case letter!\n";
+   } else {
 
   //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
-  
+    cout<<"?! Pssh. What are you talking about?\n";
+   } 
+
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  cout<<"The ASCII value is: "<<ASCII ;
 
   return 0;
 }
